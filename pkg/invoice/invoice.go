f7ef450f842697346c2dbe011497730977cf3cf1
adelson <tricker_bboy23@hotmail.com>
2023-02-20 22:40:36 -0500

    Adding packages 2

--- pkg/invoice/invoice.go
+++ pkg/invoice/invoice.go
@@ -1,7 +1,12 @@
 package invoice
 
+import (
+	"github.com/BboyGalaxy/Composition/pkg/customer"
+)
+
 type Invoice struct {
 	country string
 	city    string
 	total   float64
+	client  customer.Customer
 }
